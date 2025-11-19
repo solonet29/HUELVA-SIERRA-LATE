06744ea02a393659a0a70b446f76f35e450abd2a
Tu Nombre <hola@solonet.es>
2025-11-19 12:16:02 +0100

    He modificado loadEnv: Ahora usa process.cwd() para asegurarse de que siempre busca el archivo .env en la carpeta raíz del proyecto. He añadido un bloque define: Esta es la parte crucial. Le dice a Vite que reemplace import.meta.env en tu código con un objeto que contiene todas las variables de entorno cargadas desde tu archivo .env.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { env } from 'process';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  // Carga las variables de entorno desde el archivo .env
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // Crea un objeto para definir las variables en el cliente
+  const envWithProcessPrefix = {
+    'import.meta.env': JSON.stringify(env),
+  };
+
   return {
     server: {
       port: 3000,
@@ -17,6 +25,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     plugins: [react()],
+    define: envWithProcessPrefix,
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
